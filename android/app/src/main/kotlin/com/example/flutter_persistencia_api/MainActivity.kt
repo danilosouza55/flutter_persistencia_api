8c31e70c85d83c5680afb548cf5827a47c2545c5
Danilo Araujo de Souza <danilo.asouza@hotmail.com>
2022-05-30 22:57:25 -0300

    inicial

---
+++ android/app/src/main/kotlin/com/example/flutter_persistencia_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_persistencia_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/http/webclient.dart
@@ -0,0 +1,10 @@
+import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+import 'interceptors/logging_interceptor.dart';
+
+final Client client = InterceptedClient.build(
+  interceptors: [LoggingInterceptor()],
+);
+
+final baseUrl = Uri.http('http://192.168.20.249:8080', '/transactions');
